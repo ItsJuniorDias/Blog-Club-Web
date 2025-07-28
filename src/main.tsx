5f01d43f1aa198b169267f05726833feb2884a02
Alexandre <reabilitado97@gmail.com>
2025-07-27 21:44:14 -0700

    chore: adjust name

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+
+import { Routes, Route } from "react-router";
+
+import Home from "./screens/home/index";
+import Redirect from "./screens/redirect-app/index";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -9,7 +14,10 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <StrictMode>
-      <App />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/redirect" element={<Redirect />} />
+      </Routes>
     </StrictMode>
   </QueryClientProvider>
 );
---
+++ src/screens/home/index.tsx
@@ -0,0 +1,192 @@
+import { useEffect, useState } from "react";
+import background from "../../assets/background.png";
+
+import { addDoc, collection, db } from "../../../firebaseConfig";
+
+import Modal from "../../components/modal/index";
+
+import { useForm, Controller } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+
+const schema = z.object({
+  name: z.string("* Name is required.").min(1),
+  email: z.string("* Email is required.").email(),
+  message: z.string().optional(),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export default function HomeScreen() {
+  const [open, setOpen] = useState(false);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000); // Simula carregamento
+    return () => clearTimeout(timer);
+  }, []);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    clearErrors,
+    setValue,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "",
+      message: "",
+      name: "",
+    },
+  });
+
+  const onSubmit = async (data: FormData) => {
+    console.log("DATA FORM", data);
+
+    try {
+      const docRef = await addDoc(collection(db, "emails"), {
+        name: data.name,
+        email: data.email,
+        message: data.message,
+        createdAt: new Date(),
+      });
+
+      setOpen(true);
+
+      setValue("name", "");
+      setValue("email", "");
+      setValue("message", "");
+
+      console.log(docRef, "DOC REF");
+    } catch (e) {
+      alert(e);
+
+      console.error("Erro ao cadastrar email: ", e);
+    }
+  };
+
+  const mutation = useMutation({
+    mutationFn: handleSubmit(onSubmit),
+    onSuccess: () => {
+      // // Invalidate and refetch
+      // queryClient.invalidateQueries({ queryKey: ['todos'] })
+    },
+  });
+
+  return (
+    <>
+      <div className="min-h-screen bg-gradient-to-br from-gray-100 to-gray-300 flex items-center justify-center">
+        <div className="max-w-6xl w-full bg-white shadow-lg rounded-2xl flex overflow-hidden">
+          <div className="w-full md:w-1/2 p-10 flex flex-col justify-center">
+            <h1 className="text-4xl font-bold text-gray-800 mb-4">
+              Welcome to the <span className="text-blue-600">Blog Club</span>
+            </h1>
+
+            <p className="text-gray-600 mb-6">
+              Sign up to receive exclusive content and updates.
+            </p>
+
+            <form
+              onSubmit={(data) => mutation.mutate(data)}
+              className="space-y-4"
+            >
+              <Controller
+                name="name"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <input
+                    value={value}
+                    onChange={(text) => {
+                      onChange(text);
+
+                      if (errors.name) {
+                        clearErrors("name");
+                      }
+                    }}
+                    className={`w-full p-3 bg-[#f1f1f1] rounded-2xl  ${
+                      errors.name &&
+                      "border-2 border-red-500 focus:outline-none"
+                    } `}
+                    placeholder="Enter your name"
+                  />
+                )}
+              />
+
+              {errors.name && (
+                <div className="pb">
+                  <span className="text-red-500">{errors.name.message}</span>
+                </div>
+              )}
+
+              <Controller
+                name="email"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <input
+                    value={value}
+                    onChange={(text) => {
+                      onChange(text);
+
+                      if (errors.email) {
+                        clearErrors("email");
+                      }
+                    }}
+                    className={`w-full p-3 bg-[#f1f1f1] rounded-2xl ${
+                      errors.email &&
+                      "border-2 border-red-500 focus:outline-none"
+                    } `}
+                    placeholder="Enter your email"
+                  />
+                )}
+              />
+              {errors.email && (
+                <div className="pb">
+                  <span className=" text-red-500">{errors.email.message}</span>
+                </div>
+              )}
+
+              <Controller
+                name="message"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <textarea
+                    value={value}
+                    onChange={(text) => {
+                      onChange(text);
+
+                      if (errors.message) {
+                        clearErrors("message");
+                      }
+                    }}
+                    className="w-full p-3 bg-[#f1f1f1] rounded-2xl "
+                    placeholder="Enter a message"
+                  />
+                )}
+              />
+
+              <button
+                type="submit"
+                className="w-full bg-[#376AED] text-white px-4 py-3 rounded-2xl"
+              >
+                Send
+              </button>
+            </form>
+          </div>
+
+          <div className="hidden md:block w-1/2">
+            <img
+              src={background}
+              alt="Blog Club"
+              className="h-[787px] w-full object-cover"
+            />
+          </div>
+        </div>
+      </div>
+
+      <Modal isOpen={open} onClose={() => setOpen(false)} />
+    </>
+  );
+}
