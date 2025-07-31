4ec736733ebb4406001b20995355f6c7921771ae
Alexandre <reabilitado97@gmail.com>
2025-07-31 16:22:27 -0700

    chore: adjust hash router

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,11 @@ import { HashRouter } from "react-router-dom";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <QueryClientProvider client={queryClient}>
-      <StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <HashRouter>
         <App />
-      </StrictMode>
-    </QueryClientProvider>
-  </HashRouter>
+      </HashRouter>
+    </StrictMode>
+  </QueryClientProvider>
 );
