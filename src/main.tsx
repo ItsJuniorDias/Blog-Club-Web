96b796fd05aad9e759cbad8f2e15eb935e59c72d
Alexandre <reabilitado97@gmail.com>
2025-07-31 16:50:46 -0700

    chore: adjust browser router

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./routes/index";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <StrictMode>
-      <HashRouter>
+createRoot(document?.getElementById("root")).render(
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <StrictMode>
         <App />
-      </HashRouter>
-    </StrictMode>
-  </QueryClientProvider>
+      </StrictMode>
+    </QueryClientProvider>
+  </BrowserRouter>
 );
