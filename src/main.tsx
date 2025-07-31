be4163d6794d70974ce11444cd5505a21fd4c272
Alexandre <reabilitado97@gmail.com>
2025-07-31 16:46:50 -0700

    Revert "chore: browser router"
    This reverts commit 91187709a297ac1e7378339ad384291bb590c4e5.

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./routes/index";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </StrictMode>
   </QueryClientProvider>
 );
